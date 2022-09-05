fd7cd28bdc8a3d63aa753b2e7bc8d5694a6edebf
Marcos Marado <mindboosternoori@gmail.com>
2022-09-05 21:07:37 +0100

    Stop hardcoding end date
    Now we always retrieve data for up to 'last year', whichever that year
    might be at the time the script is ran.

--- fetch-data.sh
+++ fetch-data.sh
@@ -3,7 +3,7 @@
 apiToken=$1;
 
 echo "# info fetched from discogs at $(date)" > data.txt
-for y in $(seq 1963 2020); do
+for y in $(seq 1963 $(($(date +%Y) - 1))); do
 	cass=$(node fetch-cass-data.js "$apiToken" "$y")
 	all=$(node fetch-all-data.js  "$apiToken" "$y")
 	perc=$(echo "scale=10; 100*$cass/$all"|bc)
